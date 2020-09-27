9b96c31900ed1443a16afe9194a86b4b86aaae40
hasanZolfagharipour <hzolfagharipour@gmail.com>
2020-09-27 19:39:20 +0330

    Initial commit

---
+++ app/src/main/java/com/zolfagharipour/musicplayers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zolfagharipour.musicplayers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
